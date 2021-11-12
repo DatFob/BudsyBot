174ecc20da4959d2e2338ec6f0de16ba72d195bd
Mike Z <tzhao1@scu.edu>
2021-11-12 15:31:54 -0800

    Initial commit

---
+++ src/main/java/BotConfiguration.java
@@ -0,0 +1,19 @@
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class BotConfiguration {
+    public static JDA jda;
+
+    /**
+     * Method runs as soon as bot starts
+     */
+    public static void main(String[] args) throws LoginException{
+        //JDA Version 4.2 changed way to build, old version is:
+        //jda = new JDABuilder(AccountType.bot).setToken("").buildAsync();
+        jda = JDABuilder.createDefault(DiscordConstants.TOKEN).build();
+        jda.getPresence().setActivity(Activity.playing("Working Really Hard"));
+    }
+}
---
+++ src/main/java/DiscordConstants.java
@@ -0,0 +1,4 @@
+public class DiscordConstants {
+
+    public static final String TOKEN = "OTA3NDA5MDkxNTE1ODA1NzU2.YYmwkg.n2jMq5EQm8hlgYt7Py8ECkwPzik";
+}
